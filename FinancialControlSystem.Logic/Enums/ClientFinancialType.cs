35403f1a6a230537f86f19335f325ef89ff28f4b
Ekaterina <ekaterina_rina01@mail.ru>
2023-12-27 21:20:50 +0300

    Add Model

---
+++ FinancialControlSystem.Logic/Enums/ClientFinancialType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialControlSystem.Logic.Enums
+{
+    public enum ClientFinancialType
+    {
+        DebetCard=1,
+        CreditCard=2,
+        Cash=3,
+        BankAccount=4,
+        Debt=5
+    }
+}
---
+++ FinancialControlSystem.Logic/Models/ClientFinancialModel.cs
@@ -0,0 +1,18 @@
+﻿using FinancialControlSystem.Logic.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialControlSystem.Logic.Models
+{ 
+        public class ClientFinancialModel
+    {
+        public string Name { get; set; }
+        public string Currency { get; set; }
+        public decimal Balance { get; set; }
+        public string Comment { get; set; }
+        public ClientFinancialType Type { get; set; }
+    }
+}
